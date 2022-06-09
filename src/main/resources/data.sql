893ac740c4de0c9510b3816bb098945d1f6771ff
Lama <fhb201269@fh-vie.ac.at>
2022-06-09 21:20:01 +0200

    Changed database tables and inserted occupancy data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,25 @@
+INSERT INTO HOTEL VALUES (0, 0, 'Zum güldenen Hirsch', 'Harry Hirsch', 'Susi', 'Hirschgasse 42/47/11', 'Hirschhausen',
+                          'A-0815', '1234', 5, 50, 'www.zumgueldenenhirsch.at', false, false, true, true);
+INSERT INTO HOTEL VALUES (1, 1, 'Drei Sterne und die Sonne', 'Sternwarte', 'Hänsel', 'Sterngasse 5', 'Alldorf',
+                          'A-1234', '01234', 40, 4, 'www.dreisterne.at', false, true, false, true);
 INSERT INTO HOTEL VALUES (2, 2, 'Zum wilden Eber', 'James Bond', 'Strolchi', 'Am Berg 7', 'Großebersdorf', 'A-007',
                           '0987', 3, 3, 'www.zumwildeneber.at',true, true, true, true);
 INSERT INTO HOTEL VALUES (3, 4, 'Beim Branntweiner', 'Ima Voll', 'Gretel', 'Weinberg 3', 'Alkohütten', 'A-1234',
                           '546456', 1, 1, 'www.beimbranntweiner', true, false, true, false);
-INSERT INTO HOTEL VALUES (1, 1, 'Drei Sterne und die Sonne', 'Sternwarte', 'Hänsel', 'Sterngasse 5', 'Alldorf',
-                          'A-1234', '01234', 40, 4, 'www.dreisterne.at', false, true, false, true);
-INSERT INTO HOTEL VALUES (0, 0, 'Zum güldenen Hirsch', 'Harry Hirsch', 'Susi', 'Hirschgasse 42/47/11', 'Hirschhausen',
-                          'A-0815', '1234', 5, 50, 'www.zumgueldenenhirsch.at', false, false, true, true);
 INSERT INTO HOTEL VALUES (4, 3, 'Das ABC', 'Il. Literat', 'der böse Wolf', 'Heinzelmännchenweg 3', 'Buchstaben',
                           'A-BCDEF', '9876', 2, 2, 'www.das-abc.at', false, true, false, false);
 
+INSERT INTO OCCUPANCY VALUES (1, 1, 0, 1, 0, 2015, 0, 0);
+INSERT INTO OCCUPANCY VALUES (2, 2, 1, 2, 2, 2014, 1, 1);
+INSERT INTO OCCUPANCY VALUES (3, 3, 2, 3, 1, 2014, 2, 2);
+INSERT INTO OCCUPANCY VALUES (4, 4, 3, 5, 2, 2012, 3, 3);
+INSERT INTO OCCUPANCY VALUES (5, 5, 4, 3, 2, 2015, 4, 4);
+INSERT INTO OCCUPANCY VALUES (6, 1, 0, 0, 0, 2017, 5, 0);
+INSERT INTO OCCUPANCY VALUES (7, 2, 1, 1, 1, 2015, 6, 1);
+INSERT INTO OCCUPANCY VALUES (8, 3, 2, 4, 4, 2014, 7, 2);
+INSERT INTO OCCUPANCY VALUES (9, 4, 3, 3, 3, 2017, 8, 3);
+INSERT INTO OCCUPANCY VALUES (10, 5, 4, 6, 5, 2018, 9, 4);
+INSERT INTO OCCUPANCY VALUES (11, 1, 0, 4, 3, 2019, 10, 0);
+INSERT INTO OCCUPANCY VALUES (12, 2, 1, 3, 1, 2017, 11, 1);
+INSERT INTO OCCUPANCY VALUES (13, 3, 2, 2, 1, 2015, 12, 2);
+INSERT INTO OCCUPANCY VALUES (14, 4, 3, 2, 1, 2013, 1, 3);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,39 +1,43 @@
 /*==============================================================*/
 
-DROP TABLE IF EXISTS hotel;
+DROP TABLE IF EXISTS hotel CASCADE;
 
 /*==============================================================*/
 /* Table: hotel                                                 */
 /*==============================================================*/
 CREATE TABLE hotel
 (
-  id      INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  category int NOT NULL,
-  name    TEXT NOT NULL,
-  owner   TEXT NOT NULL,
-  contact TEXT NOT NULL,
-  address TEXT NOT NULL,
-  city    TEXT NOT NULL,
-  zip     TEXT NOT NULL,
-  phone   TEXT NOT NULL,
-  rooms   int  NOT NULL,
-  beds    int  NOT NULL,
-  url     TEXT NOT NULL,
-  family_friendly boolean NOT NULL,
-  dog_friendly boolean NOT NULL,
-  spa     boolean NOT NULL,
-  fitness boolean NOT NULL
+    id              INT     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category        INT     NOT NULL,
+    name            TEXT    NOT NULL,
+    owner           TEXT    NOT NULL,
+    contact         TEXT    NOT NULL,
+    address         TEXT    NOT NULL,
+    city            TEXT    NOT NULL,
+    zip             TEXT    NOT NULL,
+    phone           TEXT    NOT NULL,
+    rooms           INT     NOT NULL,
+    beds            INT     NOT NULL,
+    url             TEXT    NOT NULL,
+    family_friendly boolean NOT NULL DEFAULT FALSE,
+    dog_friendly    boolean NOT NULL DEFAULT FALSE,
+    spa             boolean NOT NULL DEFAULT FALSE,
+    fitness         boolean NOT NULL DEFAULT FALSE
 );
 
 DROP TABLE IF EXISTS occupancy;
 /*==============================================================*/
-/* Table: occupancy                                               */
+/* Table: occupancy                                             */
 /*==============================================================*/
 CREATE TABLE occupancy
 (
-    category TEXT NOT NULL,
-    roomCount int NOT NULL,
-    roomUtilization double NOT NULL,
-    bedCount int NOT NULL,
-    bedUtilization double NOT NULL
+    id               INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    room_count       INT NOT NULL,
+    room_utilization INT NOT NULL,
+    bed_count        INT NOT NULL,
+    bed_utilization  INT NOT NULL,
+    year             INT NOT NULL,
+    month            INT NOT NULL,
+    hotel_id         INT DEFAULT NULL,
+    FOREIGN KEY (hotel_id) REFERENCES hotel (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
